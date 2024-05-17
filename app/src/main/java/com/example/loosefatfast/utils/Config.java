64cdcc39fd2756ef519c2e901c4a9f5f3441df6e
melek <khemakhem.melek123@gmail.com>
2024-05-17 22:16:18 +0100

    change key api

--- app/src/main/java/com/example/loosefatfast/utils/Config.java
+++ app/src/main/java/com/example/loosefatfast/utils/Config.java
@@ -4,6 +4,6 @@ public class Config {
     public static final String FIREBASE_API_KEY = "your_firebase_api_key_here";
 
     // YouTube Data API Key
-    public static final String YOUTUBE_API_KEY = "AIzaSyDLFVxdyeQcglGgVR4eKfBLpqLHGw9nWqs";
+    public static final String YOUTUBE_API_KEY = "your_api_key";
 }
 
